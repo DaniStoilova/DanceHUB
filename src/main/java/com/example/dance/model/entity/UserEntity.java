7659bdfc4f4d38d1c9ef9075780323f6858ef681
DaniStoilova <113453095+DaniStoilova@users.noreply.github.com>
2024-08-21 23:06:46 +0300

    commit

---
+++ src/main/java/com/example/dance/model/entity/UserEntity.java
@@ -0,0 +1,72 @@
+package com.example.dance.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Table(name = "users")
+@Entity
+public class UserEntity extends BaseEntity {
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<RoleEntity> roles;
+
+
+    public UserEntity() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<RoleEntity> roles) {
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/com/example/dance/model/enums/RoleEnum.java
@@ -0,0 +1,6 @@
+package com.example.dance.model.enums;
+
+public enum RoleEnum {
+
+    USER, ADMIN
+}
